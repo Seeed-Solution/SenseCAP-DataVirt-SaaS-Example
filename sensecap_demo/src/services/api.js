85b756fc5812d44608d93afc271e40a2ca727801
GuLiYa <3155629926@qq.com>
2019-12-10 17:13:53 +0800

    优化样式细节

--- sensecap_demo/src/services/api.js
+++ sensecap_demo/src/services/api.js
@@ -1,4 +1,5 @@
-let apiHost = (window.location.protocol.indexOf('https') !== -1 ? 'https://' : 'http://') + 'sensecap-demo.seeed.cn:9001'
+let protocol = window.location.protocol.indexOf('https') !== -1 ? 'https://' : 'http://'
+let apiHost = protocol + (protocol === 'https://' ? 'sensecap-demo2.seeed.cn:9001' : 'sensecap-demo.seeed.cn:9001')
 let apiUrl = {
   control_api: {
     url: apiHost + '/dev/list', // 获取控制器状态
